9b6650ec32fc920db069fb61cbdc326ccd5e8874
Babatope <babathurpe@yahoo.com>
2015-02-27 08:21:43 -0500

    Initial cannon game enhancement commit

---
+++ src/com/example/androidcannongame/Line.java
@@ -0,0 +1,8 @@
+package com.example.androidcannongame;
+
+import android.graphics.Point;
+
+public class Line {
+	public Point start = new Point(); // start Point--(0,0) by default
+	public Point end = new Point(); // end Point--(0,0) by default
+}
